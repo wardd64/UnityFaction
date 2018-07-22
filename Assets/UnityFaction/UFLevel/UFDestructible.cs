09f82abce9ecc96b872d1eadd8268b27921c6187
Ward Dehairs <ward.dehairs@kuleuven.be>
2018-07-22 11:23:35 +0200

    Destructible brushes start

---
+++ Assets/UnityFaction/UFLevel/UFDestructible.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UFLevelStructure;
+
+public class UFDestructible : MonoBehaviour {
+
+    public float life;
+
+    private float damage;
+
+    public void Set(Brush brush) {
+        life = brush.life;
+    }
+
+    public void DealDamage(float amount) {
+        damage += amount;
+        if(damage >= life)
+            GetDestroyed();
+    }
+
+    private void GetDestroyed() {
+        gameObject.SetActive(false);
+    }
+	
+}
