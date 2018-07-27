813bf326f0d56a137e27dab28d8e0aa431a0a7f6
Ward Dehairs <ward.dehairs@kuleuven.be>
2018-07-27 15:46:53 +0200

    Rearranged Editor scripts for exporting

---
+++ Assets/UnityFaction/Editor/EulerAnglesDrawer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+ 
+[CustomPropertyDrawer(typeof(EulerAnglesAttribute))]
+public class EulerAnglesDrawer : PropertyDrawer {
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+        return EditorGUIUtility.singleLineHeight * (EditorGUIUtility.wideMode ? 1f : 2f);
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        Vector3 euler = property.quaternionValue.eulerAngles;
+        EditorGUI.BeginProperty(position, label, property);
+        EditorGUI.BeginChangeCheck();
+        euler = EditorGUI.Vector3Field(position, label, euler);
+        if(EditorGUI.EndChangeCheck())
+            property.quaternionValue = Quaternion.Euler(euler);
+        EditorGUI.EndProperty();
+    }
+}
